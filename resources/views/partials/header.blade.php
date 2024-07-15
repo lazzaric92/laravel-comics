02e868143d4780fa097ef1a6a670e58abd249b21
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-15 17:37:02 +0200

    added images, added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,15 @@
 <header>
-
+    <div class="container row-between-center">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Comics Logo">
+        <nav>
+            <ul class="row-center">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="#">Link</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
