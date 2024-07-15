5d1daada9ee14ff538e83e1384e67f57832032f3
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-15 18:34:38 +0200

    added main-content layout

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,24 @@
 @section('page-title', 'Homepage')
 
 @section('main-content')
-    <h1>
-        Homepage
-    </h1>
+    <section class="jumbotron">
+    </section>
+
+    <section class="main-content">
+        <div class="container column-center">
+            <div class="tag">
+                <h2>Current series</h2>
+            </div>
+            <div class="card-wrapper">
+                <article>
+                    <div class="card-image">
+                        {{-- <img :src="singleCard.thumb" :alt="singleCard.series"> --}}
+                        <h5>Card Image</h5>
+                    </div>
+                    <p> Comic title </p>
+                </article>
+            </div>
+            <button>Load more</button>
+        </div>
+    </section>
 @endsection
