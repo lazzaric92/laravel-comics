8fe3fdcaaa9156a832c71107a570f9fdf73983f2
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-15 20:39:03 +0200

    added header and footer nav links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,40 +1,147 @@
+@php
+    $navs = [
+        [
+            "title"  => 'DC Comics',
+            "links"  => [
+                [
+                    "title" => 'Characters',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Comics',
+                    "url" => '/comics'
+                ],
+                [
+                    "title" => 'Movies',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Tv',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Games',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Videos',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'News',
+                    "url" => '#'
+                ]
+            ],
+        ],
+        [
+            "title" => 'Shop',
+            "links" => [
+                [
+                    "title" => 'Shop DC',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Shop DC Collectibles',
+                    "url" => '#'
+                ]
+            ],
+        ],
+        [
+            "title" =>'DC',
+            "links" => [
+                [
+                    "title" => 'Terms Of Use',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Privacy policy (New)',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Ad Choices',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Advertising',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Jobs',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Subscriptions',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Talent Workshops',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'CPSC Certificates',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Ratings',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Shop Help',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'Contact Us',
+                    "url" => '#'
+                ]
+            ],
+        ],
+        [
+            "title" =>'Sites',
+            "links" => [
+            [
+                    "title" => 'DC',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'MAD Magazine',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'DC Kids',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'DC Universe',
+                    "url" => '#'
+                ],
+                [
+                    "title" => 'DC Power Visa',
+                    "url" => '#'
+                ]
+            ],
+        ]
+    ]
+@endphp
+
 <footer>
     <section class="footer-nav">
         <div class="container row-between-center">
             <div class="nav-wrapper">
-                <nav>
-                    <h2> Titolo </h2>
-                    <ul>
-                        <li>
-                            <a href="#">
-                                Link
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Link
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Link
-                            </a>
-                        </li>
-                    </ul>
-                    <h2> Titolo </h2>
-                    <ul>
-                        <li>
-                            <a href="#">
-                                Link
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Link
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+                @foreach($navs as $nav)
+                    <nav>
+                        <h2> {{ $nav["title"] }} </h2>
+                        <ul>
+                            @foreach ($nav["links"] as $link)
+                                <li>
+                                    <a href=" {{ $link["url"] }} ">
+                                        {{ $link["title"] }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+
+                    </nav>
+                @endforeach
             </div>
             <img src=" {{ Vite::asset('resources/img/dc-logo-bg.png')}} " alt="DC Comics Logo">
         </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,73 @@
+@php
+    $headerLinks = [
+            [
+                "title" => 'home',
+                "active" => false,
+                "url" => '/'
+            ],
+            [
+                "title" => 'characters',
+                "active" => false,
+                "url" => '#'
+            ],
+            [
+                "title" => 'comics',
+                "active" => true,
+                "url" => '/comics'
+            ],
+            [
+                "title" => 'movies',
+                "active" => false,
+                "url" => '#'
+            ],
+            [
+                "title" => 'tv',
+                "active" => false,
+                "url" => '#'
+            ],
+            [
+                "title" => 'games',
+                "active" => false,
+                "url" => '#'
+            ],
+            [
+                "title" => 'collectibles',
+                "active" => false,
+                "url" => '#'
+            ],
+            [
+                "title" => 'videos',
+                "active" => false,
+                "url" => '#'
+            ],
+            [
+                "title" => 'fans',
+                "active" => false,
+                "url" => '#'
+            ],
+            [
+                "title" => 'news',
+                "active" => false,
+                "url" => '#'
+            ],
+            [
+                "title" => 'shop',
+                "active" => false,
+                "url" => '#'
+            ]
+        ]
+@endphp
+
 <header>
     <div class="container row-between-center">
         <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Comics Logo">
         <nav>
             <ul class="row-center">
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li class="active">
-                    <a href="/comics">Comics</a>
-                </li>
+                @foreach($headerLinks as $link)
+                    <li>
+                        <a href=" {{$link["url"]}} "> {{ $link["title"] }} </a>
+                    </li>
+                @endforeach
             </ul>
         </nav>
     </div>
