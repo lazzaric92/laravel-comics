33a95894652c020b961c3ec3136c165bfb9e2eb1
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-15 17:51:31 +0200

    added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,77 @@
 <footer>
-
+    <section class="footer-nav">
+        <div class="container row-between-center">
+            <div class="nav-wrapper">
+                <nav>
+                    <h2> Titolo </h2>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Link
+                            </a>
+                        </li>
+                    </ul>
+                    <h2> Titolo </h2>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Link
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Link
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <img src=" {{ Vite::asset('resources/img/dc-logo-bg.png')}} " alt="DC Comics Logo">
+        </div>
+    </section>
+    <section class="footer">
+        <div class="container row-between-center">
+            <button>Sign-up now!</button>
+            <div class="socials row-center">
+                <h2>Follow us</h2>
+                <ul class="row-center">
+                    <li>
+                        <a href="#">
+                            <img src=" {{Vite::asset('resources/img/footer-facebook.png')}} " alt="Facebook Icon`">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src=" {{Vite::asset('resources/img/footer-twitter.png')}} " alt="Twitter Icon`">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src=" {{Vite::asset('resources/img/footer-youtube.png')}} " alt="Youtube Icon`">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src=" {{Vite::asset('resources/img/footer-periscope.png')}} " alt="Periscope Icon`">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src=" {{Vite::asset('resources/img/footer-pinterest.png')}} " alt="Pinterest Icon`">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
 </footer>
